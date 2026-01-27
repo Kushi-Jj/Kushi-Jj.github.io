09bc482f341b85e7693037407facf10ee6d13ce8
Kushi-Jj <kasunikushi@gmail.com>
2026-01-27 16:31:04 -0600

    Add toggleMenu function to manage navigation display

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+  nav.style.flexDirection = "column";
+}
